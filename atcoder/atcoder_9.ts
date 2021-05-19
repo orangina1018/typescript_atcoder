ff73a8196b0ed7e2543b46a5b0f977d20051243b
email orangina1018aaa@gmail.com <ams_ope12@AMS-PROD.local>
2021-05-19 16:23:38 +0900

    test

--- atcoder/atcoder_9.ts
+++ atcoder/atcoder_9.ts
@@ -3,7 +3,7 @@
 英小文字からなる文字列 S が与えられます。
 T が空文字列である状態から始めて、以下の操作を好きな回数繰り返すことで S=Tとすることができるか判定してください。
 
-TT の末尾に "dream", "dreamer", "erase", "eraser" のいずれかを追加する。
+T の末尾に "dream", "dreamer", "erase", "eraser" のいずれかを追加する。
 
 【制約】
 1≤|S|≤105
@@ -14,9 +14,10 @@ S は英小文字からなる
 　S = "dreameraser"
 　答え: "YES"
 
-"dream", "eraser" の順で TT の末尾に追加することで S=T とすることができます。
+"dream", "eraser" の順で T の末尾に追加することで S=T とすることができます。
 */
 
+import { reverse } from "dns";
 import * as fs from "fs";
 
 //入力を考える関数
@@ -24,6 +25,20 @@ function main(input: string) {
   // ここに処理を書く
   let S: string = input.split("\n")[0];
   console.log(S);
+
+  const string_divide_array: string[] = ["dream", "dreamer", "erase", "eraser"];
+
+  let re_reverse_string_divide_array: string[] = [];
+  for (let i = 0; i <= string_divide_array.length; ++i) {
+    let str: string = string_divide_array[i];
+    let re_str: string = reverseString(str);
+    re_reverse_string_divide_array.push(re_str);
+  }
+  console.log(re_reverse_string_divide_array)
+
+  function reverseString(str: string): string {
+    return str.split("").reverse().join("");
+  }
 }
 
 main(fs.readFileSync("input_file/file9.txt", "utf8"));
