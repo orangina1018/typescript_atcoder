deda1a4b86066d1764802f46c673cdd9f1dbb760
email orangina1018aaa@gmail.com <ams_ope12@AMS-PROD.local>
2021-05-19 15:07:14 +0900

    fix input_function

--- atcoder/atcoder_10.ts
+++ atcoder/atcoder_10.ts
@@ -21,50 +21,18 @@ AtCoDeer くんが時刻 t に 点 (x,y) にいる時、 時刻 t+1 には 点 (
 例えば (0,0),(0,1),(1,1),(1,2),(1,1),(1,0),(1,1) と移動すれば目的を果たせます。
 */
 
-import * as fs from 'fs';
+import * as fs from "fs";
 
 //入力を考える関数
 function main(input: string) {
-  // ここに処理を書く
-  let N: string = input.split('\n')[0];
-  let N_number: number = +N;
-  console.log(N);
+  let N: string = input.split("\n")[0];
+  let N_number: number = Number(N);
+  console.log(N_number);
 
-  let A: string = input.split('\n')[1];
-  let A_number: number = +A;
-  console.log(A);
-
-  let B: string = input.split('\n')[2];
-  let B_number: number = +B;
-  console.log(B);
-
-  sumMain(N_number, A_number, B_number);
+  // 後で訂正
+  let A_array: string[] = input.split("\n")[1].split(",");
+  let A_number_array: number = Number(A_array);
+  console.log(A_number_array);
 }
 
-//格桁の総和を求める
-function sumDigitnumber(X: number): number {
-  let remainder: number = 0;
-  X = 0;
-
-  while (X > 0) {
-    remainder += X % 10;
-    X /= 10;
-  }
-  let sum = remainder;
-  return sum;
-}
-
-console.log();
-
-function sumMain(N_number: number, A_number: number, B_number: number): void {
-  let ans_total_number: number = 0;
-  for (let i = 1; i <= N_number; ++i) {
-    let sum = sumDigitnumber(i);
-    if (sum >= A_number && sum <= B_number) {
-      ans_total_number += i;
-    }
-  }
-  return console.log(ans_total_number);
-}
-
-main(fs.readFileSync('input_file/file5.txt', 'utf8'));
+main(fs.readFileSync("input_file/file10.txt", "utf8"));
--- atcoder/atcoder_9.ts
+++ atcoder/atcoder_9.ts
@@ -17,50 +17,13 @@ S は英小文字からなる
 "dream", "eraser" の順で TT の末尾に追加することで S=T とすることができます。
 */
 
-import * as fs from 'fs';
+import * as fs from "fs";
 
 //入力を考える関数
 function main(input: string) {
   // ここに処理を書く
-  let N: string = input.split('\n')[0];
-  let N_number: number = +N;
-  console.log(N);
-
-  let A: string = input.split('\n')[1];
-  let A_number: number = +A;
-  console.log(A);
-
-  let B: string = input.split('\n')[2];
-  let B_number: number = +B;
-  console.log(B);
-
-  sumMain(N_number, A_number, B_number);
+  let S: string = input.split("\n")[0];
+  console.log(S);
 }
 
-//格桁の総和を求める
-function sumDigitnumber(X: number): number {
-  let remainder: number = 0;
-  X = 0;
-
-  while (X > 0) {
-    remainder += X % 10;
-    X /= 10;
-  }
-  let sum = remainder;
-  return sum;
-}
-
-console.log();
-
-function sumMain(N_number: number, A_number: number, B_number: number): void {
-  let ans_total_number: number = 0;
-  for (let i = 1; i <= N_number; ++i) {
-    let sum = sumDigitnumber(i);
-    if (sum >= A_number && sum <= B_number) {
-      ans_total_number += i;
-    }
-  }
-  return console.log(ans_total_number);
-}
-
-main(fs.readFileSync('input_file/file5.txt', 'utf8'));
+main(fs.readFileSync("input_file/file9.txt", "utf8"));
